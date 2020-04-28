eda732d3a6f97e3a18a0b9b3309db7737623b4a8
Jean-Baptiste Queru <jbqueru@gmail.com>
2020-04-27 20:28:18 -0700

    Consolidate font at top of spritesheet

--- mono.lua
+++ mono.lua
@@ -1,10 +1,6 @@
 function _draw()
 	cls(0)
-	intro_text = "HELLO WORLD"
-	for x=1,#intro_text do
-		spr(ord(intro_text,x),8*(x-1),0)
-	end
-	map(0,0,0,8,11,1)
+	map(0,0,0,0,11,1)
 end
 
 function _update60()
